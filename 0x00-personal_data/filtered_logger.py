a30bd14c1b2eeec3555e9a30ff8ac0470e85303c
habibafernas <habibafernas@gmail>
2024-08-02 19:48:25 +0300

    a

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,13 @@ import mysql.connector
 from typing import List
 
 
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
         ) -> str:
